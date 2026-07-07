d5668fb823800a70c7faaa3c24bf970ffbe5a546
bry402 <bry402.ofc@gmail.com>
2026-07-06 22:00:40 -0300

    Added require support

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+local _, path_to_module = ...
+local module_dir = path_to_module:match(".*/")
+package.path = package.path..";"..module_dir.."?.lua;"..module_dir.."?/init.lua"
+-- voodoo magic type shit
+
 local place = require("libs/place")
 local Classes = require("Classes")
 
